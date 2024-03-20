5d8b67efe797fe5ab6d48d1eccf7b7be61961807
huynhvantam <huynhvantamm@gmail.com>
2024-03-20 16:23:21 +0700

    header

--- app/[lng]/ve-chung-toi/tam-nhin-su-menh/page.jsx
+++ app/[lng]/ve-chung-toi/tam-nhin-su-menh/page.jsx
@@ -43,35 +43,7 @@ export default async function Page({ params: { lng } }) {
         text2="Tầm nhìn - Sứ mệnh"
       />
       <Pagedautien links={links} highlight={highlight} />
-      <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-        <div className="md:w-1/4 py-64 md:mb-0 mb-6 flex flex-col text-center items-center">
-          <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-orange-100 text-orange-500 mb-5 flex-shrink-0">
-            <img id="pre-btn" alt="logo" className="h-10 pr-0" src={back}></img>
-          </div>
-        </div>
-        <div className="md:w-2/4 md:mb-0 mb-6 flex flex-col text-center items-center">
-          <section
-            id="slider"
-            className="w-16 h-20 inline-flex items-center justify-center mb-5 flex-shrink-0"
-          >
-            <input type="radio" name="slider" id="s1" defaultChecked="false" />
-            <input type="radio" name="slider" id="s2" defaultChecked="true" />
-            <input type="radio" name="slider" id="s3" defaultChecked="false" />
-            <label htmlFor="s1" id="slide1">
-              {/* <img className="fea" src="./assets/img/img1.jpg" height="100%" width="100%"/> */}
-            </label>
-            <label htmlFor="s2" id="slide2">
-              {/* <img className="fea" src="./assets/img/img2.jpg" height="100%" width="100%"/> */}
-            </label>
-            <label htmlFor="s3" id="slide3">
-              {/* <img className="fea" src="./assets/img/img3.jpg" height="100%" width="100%"/> */}
-            </label>
-          </section>
-        </div>
-        <div className="md:w-1/4 py-64 md:mb-0 mb-6 flex flex-col text-center items-center">
-          <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-orange-100 text-orange-500 mb-5 flex-shrink-0"></div>
-        </div>
-      </div>
+
       <section className="max-w-6xl mx-auto px-4 sm:px-6 ">
         <h2 className="pt-20 h3  text-center border-b-[3px] border-b-rose-500 pb-3 mb-6 w-fit mx-auto">
           THÔNG ĐIỆP
